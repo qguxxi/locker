2c9740c922bc28fa443909032a33579137f4c379
Duc Hoang Manh <jandyz.dev@gmail.com>
2025-06-27 14:41:13 +0700

    implement clean OnBoardingScreen with CTAButton hoisting

--- app/src/main/java/com/opentech/locker/presentation/ui/screen/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/opentech/locker/presentation/ui/screen/onboarding/OnBoardingScreen.kt
@@ -1,14 +1,17 @@
 package com.opentech.locker.presentation.ui.screen.onboarding
 
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
